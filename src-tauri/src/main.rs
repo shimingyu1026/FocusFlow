3c3561e92874011f954782ffd57682972edea6e6
shimingyu <smy.yms@foxmail.com>
2026-05-21 12:16:52 +0800

    config: tighten tauri permissions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -16,7 +16,6 @@ pub fn run() {
     let timer_state = TimerState::new();
 
     tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .manage(timer_state)
